f4479cc2e4a3b083c35e0142c20379961d6b46b9
dditthardt <dditthardt@a1c503db-2e5a-4881-8658-c54c93b95141>
2010-10-11 23:50:33 +0000

    Added very basic character entity
    git-svn-id: https://subversion.assembla.com/svn/v2engine/trunk@9 a1c503db-2e5a-4881-8658-c54c93b95141

---
+++ Server/Entities/Character.class.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Entities;
+
+
+/**
+ * Character Class
+ */
+class Character
+{
+	/**
+	 * Id
+	 *
+	 * 28 character identifier
+	 *
+	 * @var characterId
+	 */
+	public Id;
+
+	/**
+	 * First Name
+	 *
+	 * @var FirstName
+	 */
+	public FirstName;
+
+	/**
+	 * Middle Name
+	 *
+	 * @var MiddleName
+	 */
+	public MiddleName;
+
+	/**
+	 * Last Name
+	 *
+	 * @var LastName
+	 */
+	public LastName;
+
+	/**
+	 * CreatedOn
+	 *
+	 * The date the character was created
+	 *
+	 * @var createdOn
+	 */
+	public CreatedOn;
+
+	/**
+	 * Default constructor for the Account Class
+	 */
+	public function __construct()
+	{
+		
+	}
+}
+
+?>
