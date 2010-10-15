3eb70397a1e3443379ea7a26fcae4200fc99e545
dditthardt <dditthardt@a1c503db-2e5a-4881-8658-c54c93b95141>
2010-10-15 20:50:12 +0000

    removed constructor from account entity
    git-svn-id: https://subversion.assembla.com/svn/v2engine/trunk@31 a1c503db-2e5a-4881-8658-c54c93b95141

--- Server/Entities/Account.class.php
+++ Server/Entities/Account.class.php
@@ -66,35 +66,6 @@ class Account
 		
 	}
 
-
-	/**
-	 * Constructor for the Account Class
-	 *
-	 * @param $Id
-	 *   The account's 28 character string Identifier
-	 *
-	 * @param $Name
-	 *   The account's Name
-	 *
-	 * @param $Type
-	 *   The Type of account. 0 = banned, 1 = normal, 2 = admin
-	 *
-	 * @param $Validated
-	 *   Whether the account's email has been validated or not.
-	 *
-	 * @param $CreatedOn
-	 *   Timestamp when the account was created
-	 */
-	public function __construct($Id, $Name, $Password, $Type, $Validated, $CreatedOn)
-	{
-		$this->Id = $Id;
-		$this->Name = $Name;
-		$this->Password = $Password;
-		$this->Type = $Type;
-		$this->Validated = $Validated;
-		$this->CreatedOn = $CreatedOn;
-	}
-
 	/**
 	 * Verifies account data, ensures all fields are valid.
 	 */
