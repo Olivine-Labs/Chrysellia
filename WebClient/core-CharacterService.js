f772b202bd9e17f30b64630b3151b11f5ccb2065
ajacksified <ajacksified@a1c503db-2e5a-4881-8658-c54c93b95141>
2010-10-18 18:57:58 +0000

    client-side character services and tests
    git-svn-id: https://subversion.assembla.com/svn/v2engine/trunk@45 a1c503db-2e5a-4881-8658-c54c93b95141

---
+++ WebClient/core-CharacterService.js
@@ -0,0 +1,58 @@
+/*!
+ * V2 Account Services Library 0.0.1 rev 01
+ * http://v2.neflaria.com/
+ *
+ * Copyright 2010, Jack Lawson
+ *
+ * Date: Tue Oct 12 08:34:00 [-0500]
+ *
+ *Dependencies: jQuery Core, core.js
+ */
+ 
+(function( window, undefined ) {
+	window.ACTION_CREATE = 0;
+	window.ACTION_LIST = 1;
+	window.ACTION_CHECKNAME = 2;
+	
+	var CharacterService = function (){};
+	
+	CharacterService = CharacterService.prototype = {
+		Create: function(firstName, middleName, lastName, raceID, strength, dexterity, intelligence, wisdom, vitality, callback){
+			$.ajax({
+				url: SERVERCODE_DIRECTORY + "Character.php",
+				cache: false,
+				type: "POST",
+				data: { Action: LOGIN, Data: JSON.stringify({ FirstName: firstName, MiddleName: middleName, LastName: lastName, Strength: strength, Dexterity: dexterity, Intelligence: intelligence, Wisdom: wisdom, Vitality: vitality }) },
+				success: function(response){
+					callback(response);
+			   }
+			});
+		},
+		
+		List: function(callback){
+			$.ajax({
+				url: SERVERCODE_DIRECTORY + "Character.php",
+				cache: true,
+				type: "POST",
+				data: { Action: REGISTER, Data: JSON.stringify({}) },
+				success: function(response){
+					callback(response);
+			   }
+			});
+		},
+		
+		CheckName: function(firstName, middleName, lastName, callback){
+			$.ajax({
+				url: SERVERCODE_DIRECTORY + "Character.php",
+				cache: false,
+				type: "POST",
+				data: { Action: REGISTER, Data: JSON.stringify({ FirstName: firstName, MiddleName: middleName, LastName: lastName }) },
+				success: function(response){
+					callback(response);
+			   }
+			});
+		}
+	}
+	
+	V2Core.CharacterService = V2Core.cs = CharacterService;
+})(window);
