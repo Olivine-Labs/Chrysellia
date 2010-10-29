f51ccbc099eaaccd18f0b4fd2b27e87d39604f2e
ajacksified <ajacksified@a1c503db-2e5a-4881-8658-c54c93b95141>
2010-10-29 21:57:28 +0000

    Working on latest design (unfinished)
    git-svn-id: https://subversion.assembla.com/svn/v2engine/trunk@109 a1c503db-2e5a-4881-8658-c54c93b95141

---
+++ WebClient/index.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+
+<html lang="en">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html;charset=utf-8" /> 
+		<meta name="keywords" content="free online mmo, mmorpg, rpg, neflaria, shimlar, neflaria v2" />
+		<meta name="description" content="Neflaria V2 is a free online RPG: create an account and fight for the top spot!" />
+
+		<meta name="author" content="silwarnaiilo@neflaria.com" />
+		<meta name="distribution" content="Global" />
+		<meta name="copyright" content="All content copyright 2010 Chrysolite Foundation. All rights reserved." />
+		<!-- todo: remove for beta -->
+		<meta name="robots" content="nofollow" />
+		
+		<title>Neflaria V2</title>
+		
+		<!--[if IE]>
+			<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		
+		<link href='http://fonts.googleapis.com/css?family=Crimson+Text&subset=latin' rel='stylesheet' type='text/css'>
+		
+		<link href="css/html5-reset.css" rel="stylesheet" media="screen" />
+		<link href="css/jquery-ui.css" rel="stylesheet" media="screen" />
+		<link href="css/grid.css" rel="stylesheet" media="screen" />
+		<link href="css/neflaria-base.css" rel="stylesheet" media="screen" />
+		
+		<!--[if IE 8]>
+		<link href="css/ie8.css" rel="stylesheet" media="screen" />
+		<![endif]-->
+		
+		<!--[if IE 7]>
+		<link href="css/ie7.css" rel="stylesheet" media="screen" />
+		<![endif]-->
+		
+		<!--[if IE 6]>
+		<link href="css/ie6.css" rel="stylesheet" media="screen" />
+		<![endif]-->
+		
+		<link rel="icon" type="image/png" href="images/favicon.ico" /> 
+		<link rel="alternate" type="application/rss+xml" title="Neflaria News RSS Feed" href="http://v2.neflaria.com/blog/feed/" />
+	</head>
+	<body>
+		<div id="messages">
+			<div class="container_12">
+				<div class="grid_12" id="info">
+					<span class="info">Online: <span id="onlines"></span></span>
+					<span class="info">@<a href="#">Neflaria</a>: <span id="onlines"></span></span>
+					<span class="info">News: <a href="#">We've updated the graphics, and redeveloped some...</a></span>
+				</div>
+			</div>
+			<div class="clear"></div>
+		</div>
+		
+		<div id="navigation">
+			<div class="container_12">
+				<div class="grid_12" id="mainNav">
+					<nav>
+						<ul>
+							<li><a href="index.html" class="selected">Home</a></li>
+							<li><a href="http://v2.neflaria.com">Blog</a></li>
+							<li><a href="#">Manual</a></li>
+							<li><a href="#">Tops Lists</a></li>
+							<li><a href="#">Forum</a></li>
+							<li><a href="#">Wiki</a></li>
+							<li><a href="#">Donate</a></li>
+						</ul>
+					</nav>
+					
+					<div class="quickLogin">
+						<form action="submitaction.php" method="post">
+							<input type="text" placeholder="Username" name="quickLogin_un" id="quickLogin_un" />
+							<input type="password" placeholder="Password" name="quickLogin_pw" id="quickLogin_pw" />
+							<input type="hidden" value="login" name="action" id="action" />
+							
+							<button class="button">Log in</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<section id="top10" class="container_12">
+			<div class="grid_12">
+			<h1>Top 25 (Level):</h1>
+				<ul>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+				</ul>
+			</div>
+		</section>
+		
+		<div class="container_12">
+			<div class="mainContainer">
+				<div class="container_8">
+					<div class="grid_8">
+						<header class="pageHeader">
+							<h1>Neflaria</h1>
+							<h2>The classic free online RPG</h2>
+							<a class="button bigButton" href="#">Play Now!</a>
+						</header>
+					</div>
+					
+					<div class="clear"></div>
+					
+					<section class="grid_4 news">
+						<h1>News</h1>
+						<section class="mainNews newsItem">
+							<h1>Neflaria Beta Open</h1>
+							<em class="alternate"><date></date> - by <a href="#">ajacksified</a></em>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla dignissim vehicula risus, vel scelerisque erat fermentum quis. Ut aliquam, augue pharetra facilisis dignissim, mauris lectus hendrerit erat, nec dapibus purus arcu id est. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vulputate faucibus purus, eget egestas justo semper vitae. Nam velit neque, pulvinar vitae rhoncus luctus, ullamcorper at augue.</p>
+							<a href="#" class="alternate">Neflaria Developer Blog</a>
+						</section>
+						
+						<section class="secondaryNews newsItem">
+							<h1>More News</h1>
+							<ul>
+								<li class="alternate"><a href="#">Neflaria Developer Blog</a> - (<date></date>)</li>
+								<li class="alternate"><a href="#">Neflaria Developer Blog</a> - (<date></date>)</li>
+								<li class="alternate"><a href="#">Neflaria Developer Blog</a> - (<date></date>)</li>
+								<li class="alternate"><a href="#">Neflaria Developer Blog</a> - (<date></date>)</li>
+							</ul>
+							<a href="#" class="alternate">Subscribe to RSS</a> - <a href="#" class="alternate">Archive</a>
+						</section>
+					</section>
+					
+					<section class="grid_4 fromManual">
+						<h1>From the Manual</h1>
+						<h2>Race Choices</h2>
+						<p>Nulla molestie neque vitae est volutpat a viverra dui commodo. Aliquam commodo tempor vehicula. Vestibulum tincidunt, urna condimentum commodo bibendum, urna dolor laoreet ante, sit amet adipiscing mi velit quis justo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Proin convallis turpis nec quam dignissim nec vehicula dolor adipiscing.</p>
+						<a href="#" class="alternate">Visit the Manual</a>
+					</section>
+				</div>
+				
+				<div class="grid_4 goodx"></div>
+				
+				<div class="clear"></div>
+			</div>
+		</div>
+		
+		<footer class="copyright">
+			<div class="container_12">
+				&copy; 2010 <a href="#">Chrysolite Foundation</a>, all rights reserved. <a href="#">Terms of Service</a> - <a href="#">Privacy Policy</a> - <a href="#">Contact</a>
+				<br />
+				Neflaria is built on the <a href="#">Chrysolite Game Engine</a>, available under <a href="#">BSD Licensing</a>.
+			</div>
+		</footer>
+	</body>
+</html>
