3ee71c694dc46f14864ba9d35e47142a4e3285d6
dditthardt <dditthardt@a1c503db-2e5a-4881-8658-c54c93b95141>
2010-11-03 18:44:47 +0000

    Fixed issue number 2.
    Part now returns success correctly when the command works.
    
    git-svn-id: https://subversion.assembla.com/svn/v2engine/trunk@126 a1c503db-2e5a-4881-8658-c54c93b95141

--- Server/Functions/Commands/ChannelPart.php
+++ Server/Functions/Commands/ChannelPart.php
@@ -14,9 +14,14 @@ if(property_exists($Post, 'Channel'))
 	$Character = new \Entities\Character();
 	$Character->CharacterId = $_SESSION['CharacterId'];
 	if($Database->Chat->LeaveChannel($Character, $Post->Channel))
+	{
 		unset($_SESSION['Channels'][$ChannelId]);
-	else
 		$Result->Set('Result', \Protocol\Result::ER_BADDATA);
+	}
+	else
+	{
+		$Result->Set('Result', \Protocol\Result::ER_BADDATA);
+	}
 }
 else
 {
