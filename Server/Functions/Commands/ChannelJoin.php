bc85d207deb248106f751481c4c50af9c6284016
dditthardt <dditthardt@a1c503db-2e5a-4881-8658-c54c93b95141>
2010-11-09 18:00:23 +0000

    Fixed bug in channel join
    git-svn-id: https://subversion.assembla.com/svn/v2engine/trunk@143 a1c503db-2e5a-4881-8658-c54c93b95141

--- Server/Functions/Commands/ChannelJoin.php
+++ Server/Functions/Commands/ChannelJoin.php
@@ -18,7 +18,7 @@ if(property_exists($Get, 'Channel'))
 		if($Channel = $Database->Chat->JoinChannel($Character, $Get->Channel))
 		{
 			$Result->Set('Result', \Protocol\Result::ER_SUCCESS);
-			$Result->Set('Data', Array('ChannelId'=>$Channel));
+			$Result->Set('Data', $Channel);
 			$_SESSION['Channels'][$Channel["ChannelId"]] = new stdClass();
 			$_SESSION['Channels'][$Channel["ChannelId"]]->LastRefresh = time() - 300;
 		}
