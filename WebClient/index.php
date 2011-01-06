8ac31df1f96ad75c15c620f683a5cc39a6b5dd17
dditthardt <dditthardt@a1c503db-2e5a-4881-8658-c54c93b95141>
2011-01-06 00:14:10 +0000

    Fixed facebook error messages on main page when not logged in.
    git-svn-id: https://subversion.assembla.com/svn/v2engine/trunk@228 a1c503db-2e5a-4881-8658-c54c93b95141

--- WebClient/index.php
+++ WebClient/index.php
@@ -3,20 +3,27 @@
 define('FACEBOOK_APP_ID', '119442588120693');
 define('FACEBOOK_SECRET', '394479c493bac0ee777a8d9cf61ac4fd');
 
-function get_facebook_cookie($app_id, $application_secret) {
-  $args = array();
-  parse_str(trim($_COOKIE['fbs_' . $app_id], '\\"'), $args);
-  ksort($args);
-  $payload = '';
-  foreach ($args as $key => $value) {
-    if ($key != 'sig') {
-      $payload .= $key . '=' . $value;
-    }
-  }
-  if (md5($payload . $application_secret) != $args['sig']) {
-    return null;
-  }
-  return $args;
+function get_facebook_cookie($app_id, $application_secret)
+{
+	$args = array();
+	if(isset($_COOKIE['fbs_' . $app_id]))
+	{
+		parse_str(trim($_COOKIE['fbs_' . $app_id], '\\"'), $args);
+		ksort($args);
+		$payload = '';
+		foreach ($args as $key => $value)
+		{
+			if ($key != 'sig')
+			{
+				$payload .= $key . '=' . $value;
+			}
+		}
+		if (md5($payload . $application_secret) != $args['sig'])
+		{
+			return null;
+		}
+	}
+	return $args;
 }
 
 $cookie = get_facebook_cookie(FACEBOOK_APP_ID, FACEBOOK_SECRET);
