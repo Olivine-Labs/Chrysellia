c366d2bc6b5edd296b7b44426b969612b8e81402
dditthardt <dditthardt@a1c503db-2e5a-4881-8658-c54c93b95141>
2011-01-08 08:48:58 +0000

    Completed implementation of levelup
    git-svn-id: https://subversion.assembla.com/svn/v2engine/trunk@291 a1c503db-2e5a-4881-8658-c54c93b95141

--- Server/Functions/Character/LevelUp.php
+++ Server/Functions/Character/LevelUp.php
@@ -22,7 +22,47 @@ try
 			{
 				if($Character->FreeLevels > 0)
 				{
-					$Result->Set('Result', \Protocol\Result::ER_SUCCESS);
+					switch($Get->Stat)
+					{
+						case 0:
+							$Character->Strength += 10;
+							$Character->Dexterity += 10;
+							$Character->Vitality += 10;
+							$Character->Intelligence += 10;
+							$Character->Wisdom += 10;
+							break;
+						case 1:
+							$Character->Strength += 50;
+							break;
+						case 2:
+							$Character->Dexterity += 50;
+							break;
+						case 3:
+							$Character->Vitality += 50;
+							break;
+						case 4:
+							$Character->Intelligence += 50;
+							break;
+						case 5:
+							$Character->Wisdom += 50;
+							break;
+					}
+
+					$Character->Strength += $Character->RacialStrength;
+					$Character->Dexterity += $Character->RacialDexterity;
+					$Character->Vitality += $Character->RacialVitality;
+					$Character->Intelligence += $Character->RacialIntelligence;
+					$Character->Wisdom += $Character->RacialWisdom;
+
+					$Character->FreeLevels -= 1;
+					if($Database->Characters->UpdateTraits($Character))
+					{
+						$Result->Set('Result', \Protocol\Result::ER_SUCCESS);
+					}
+					else
+					{
+						$Result->Set('Result', \Protocol\Result::ER_DBERROR);
+					}
 				}
 			}
 			else
