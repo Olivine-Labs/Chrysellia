3d5a1c7de134a39c49aa8d876906c8a810968b88
dditthardt <dditthardt@a1c503db-2e5a-4881-8658-c54c93b95141>
2011-01-09 06:51:55 +0000

    Stubbed shop functions
    git-svn-id: https://subversion.assembla.com/svn/v2engine/trunk@314 a1c503db-2e5a-4881-8658-c54c93b95141

---
+++ Server/Functions/Places/Sell.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Sell Logic
+ */
+
+$Get = (object)Array('Data'=>'');
+if(isset($_GET['Data']))
+{
+	$Get = json_decode($_GET['Data']);
+}
+
+if(
+	property_exists($Get, 'ItemTemplateId')
+){
+	$Character = new \Entities\Character();
+	$Character->CharacterId = $_SESSION['CharacterId'];
+	if($Database->Characters->LoadTraits($Character))
+	{
+		//TODO
+	}
+	else
+	{
+		$Result->Set('Result', \Protocol\Result::ER_DBERROR);
+	}
+}
+else
+{
+	$Result->Set('Result', \Protocol\Result::ER_MALFORMED);
+}
+
+?>
