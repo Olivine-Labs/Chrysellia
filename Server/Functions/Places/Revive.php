d9146433bf9e1df0114703a394e37db92b2a01f7
dditthardt <dditthardt@a1c503db-2e5a-4881-8658-c54c93b95141>
2011-01-10 06:03:39 +0000

    fixed bugs in revive
    git-svn-id: https://subversion.assembla.com/svn/v2engine/trunk@329 a1c503db-2e5a-4881-8658-c54c93b95141

--- Server/Functions/Places/Revive.php
+++ Server/Functions/Places/Revive.php
@@ -26,15 +26,18 @@ if($LastReviveTime + 15 * $RevivePenaltyMultiplier < time())
 		{
 			if($Cell['PlaceId'] == 'PLAC_00000000000000000000002')
 			{
-				$Character->Health = $Character->Vitality;
-				if($Database->Character->UpdateTraits($Character))
+				if($Character->Health <=0)
 				{
-					$Result->Set('Result', \Protocol\Result::ER_SUCCESS);
-					$_SESSION['LastReviveTime'] = time();
-				}
-				else
-				{
-					$Result->Set('Result', \Protocol\Result::ER_DBERROR);
+					$Character->Health = $Character->Vitality;
+					if($Database->Characters->UpdateTraits($Character))
+					{
+						$Result->Set('Result', \Protocol\Result::ER_SUCCESS);
+						$_SESSION['LastReviveTime'] = time();
+					}
+					else
+					{
+						$Result->Set('Result', \Protocol\Result::ER_DBERROR);
+					}
 				}
 			}
 		}
