087c4dc57a25fab76e1aa9d32657d9bbe225b215
dditthardt <dditthardt@a1c503db-2e5a-4881-8658-c54c93b95141>
2011-01-10 06:43:58 +0000

    Implemented banks serverside
    git-svn-id: https://subversion.assembla.com/svn/v2engine/trunk@331 a1c503db-2e5a-4881-8658-c54c93b95141

---
+++ Server/Functions/Places/GoldTransfer.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Transfer Logic
+ */
+
+$Get = (object)Array('Data'=>'');
+if(isset($_GET['Data']))
+{
+	$Get = json_decode($_GET['Data']);
+}
+
+if(
+	property_exists($Get, 'Gold') &&
+	property_exists($Get, 'Name')
+){
+	$Character = new \Entities\Character();
+	$Character->CharacterId = $_SESSION['CharacterId'];
+	if($Database->Characters->LoadTraits($Character) && $Database->Characters->LoadPosition($Character))
+	{
+		if($Cell = $Database->Maps->GetCell($Character->MapId, $Character->PositionX, $Character->PositionY))
+		{
+			if($Cell['PlaceId'] == 'PLAC_00000000000000000000003')
+			{
+				if($Character->Bank > $Get->Gold)
+				{
+					$TargetCharacter = new \Entities\Character();
+					$TargetCharacter->Name = $Get->Name;
+					if($Database->Characters->CheckName($TargetCharacter))
+					{
+						if($Database->Characters->LoadTraits($TargetCharacter))
+						{
+							$TargetCharacter->Bank += $Get->Gold;
+							$Character->Bank -= $Get->Gold;
+							if($Database->Characters->UpdateTraits($Character) && $Database->Characters->UpdateTraits($TargetCharacter))
+							{
+								$Result->Set('Result', \Protocol\Result::ER_SUCCESS);
+							}
+							else
+							{
+								$Result->Set('Result', \Protocol\Result::ER_DBERROR);
+							}
+						}
+						else
+						{
+							$Result->Set('Result', \Protocol\Result::ER_DBERROR);
+						}
+					}
+					else
+					{
+						$Result->Set('Result', \Protocol\Result::ER_BADDATA);
+					}
+				}
+			}
+		}
+		else
+		{
+			$Result->Set('Result', \Protocol\Result::ER_DBERROR);
+		}
+	}
+	else
+	{
+		$Result->Set('Result', \Protocol\Result::ER_DBERROR);
+	}
+}
+else
+{
+	$Result->Set('Result', \Protocol\Result::ER_MALFORMED);
+}
+?>
---
+++ Server/Functions/Places/Withdraw.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Withdraw Logic
+ */
+
+$Get = (object)Array('Data'=>'');
+if(isset($_GET['Data']))
+{
+	$Get = json_decode($_GET['Data']);
+}
+
+if(
+	property_exists($Get, 'Gold')
+){
+	$Character = new \Entities\Character();
+	$Character->CharacterId = $_SESSION['CharacterId'];
+	if($Database->Characters->LoadTraits($Character) && $Database->Characters->LoadPosition($Character) && $Database->Characters->LoadById($Character))
+	{
+		if($Cell = $Database->Maps->GetCell($Character->MapId, $Character->PositionX, $Character->PositionY))
+		{
+			if($Cell['PlaceId'] == 'PLAC_00000000000000000000003')
+			{
+				if($Character->Bank > $Get->Gold)
+				{
+					$Character->Bank -= $Get->Gold;
+					$Character->Gold += $Get->Gold;
+					if($Database->Characters->UpdateTraits($Character))
+					{
+						$Result->Set('Result', \Protocol\Result::ER_SUCCESS);
+					}
+					else
+					{
+						$Result->Set('Result', \Protocol\Result::ER_DBERROR);
+					}
+				}
+			}
+		}
+		else
+		{
+			$Result->Set('Result', \Protocol\Result::ER_DBERROR);
+		}
+	}
+	else
+	{
+		$Result->Set('Result', \Protocol\Result::ER_DBERROR);
+	}
+}
+else
+{
+	$Result->Set('Result', \Protocol\Result::ER_MALFORMED);
+}
+?>
