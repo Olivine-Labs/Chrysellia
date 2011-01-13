fe821f00b22da5f0dc235a319c5f97265f101adf
dditthardt <dditthardt@a1c503db-2e5a-4881-8658-c54c93b95141>
2011-01-13 05:43:04 +0000

    Added API function
    git-svn-id: https://subversion.assembla.com/svn/v2engine/trunk@414 a1c503db-2e5a-4881-8658-c54c93b95141

---
+++ Server/API.php
@@ -0,0 +1,36 @@
+<?php
+header('Cache-Control: no-cache, must-revalidate');
+header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+header('Content-type: application/json');
+
+require('./Common/Common.inc.php');
+$Result = new \Protocol\Result();
+$Database->Log = $Result;
+
+if ( 'GET' == $_SERVER['REQUEST_METHOD'] )
+{
+		define('ACTION_TOP', 0);
+		define('ACTION_ONLINE', 0);
+		if(isset($_GET['Action']))
+		{
+			switch($_GET['Action'])
+			{
+				case ACTION_TOP:
+					include './Functions/API/Top.php';
+					break;
+				case ACTION_ONLINE:
+					include './Functions/API/Online.php';
+					break;
+				default:
+					$Result->Set('Result', \Protocol\Result::ER_BADDATA);
+					break;
+			}
+		}
+		else
+		{
+			$Result->Set('Result', \Protocol\Result::ER_MALFORMED);
+		}
+	}
+}
+$Result->Output();
+?>
