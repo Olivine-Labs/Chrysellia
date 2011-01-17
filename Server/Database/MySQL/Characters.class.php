a89eb855694fcb10817476bd1dcb3df8e559d8a3
dditthardt <dditthardt@a1c503db-2e5a-4881-8658-c54c93b95141>
2011-01-17 04:35:33 +0000

    git-svn-id: https://subversion.assembla.com/svn/v2engine/trunk@441 a1c503db-2e5a-4881-8658-c54c93b95141

--- Server/Database/MySQL/Characters.class.php
+++ Server/Database/MySQL/Characters.class.php
@@ -26,7 +26,7 @@ define('SQL_INSERTCHARACTERLOCATION', 'INSERT INTO `character_locations` (`chara
 define('SQL_LOADLISTFORCELL', 'SELECT c.characterId, c.name, ct.gender, ct.raceId, c.clanId, ct.level, ct.alignGood, ct.alignOrder FROM `characters` c INNER JOIN `character_locations` cl ON cl.characterId=c.characterId INNER JOIN `character_traits` ct ON ct.characterId=c.characterId WHERE cl.mapId=? AND cl.positionX=? AND cl.positionY=? AND ct.Health > 0');
 
 //API
-define('SQL_LOADTOPLISTASC', 'SELECT c.name, ct.gender, ct.raceId, c.clanId, ct.level, ct.alignGood, ct.alignOrder FROM `characters` c INNER JOIN `character_traits` ct ON ct.characterId=c.characterId');
+define('SQL_LOADTOPLIST', 'SELECT c.name, ct.gender, ct.raceId, c.clanId, ct.level, ct.alignGood, ct.alignOrder FROM `characters` c INNER JOIN `character_traits` ct ON ct.characterId=c.characterId');
 define('SQL_LIMIT', ' LIMIT ?, ?');
 define('SQL_ORDERBY', ' ORDER BY ');
 define('SQL_GETCOUNT', 'SELECT count(*) FROM `characters`');
