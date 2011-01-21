af9dbb5253e259d55d0d974badc33ad0dfcc51d7
BrokenWits <BrokenWits@a1c503db-2e5a-4881-8658-c54c93b95141>
2011-01-21 03:28:13 +0000

    Merged content files into Branch
    git-svn-id: https://subversion.assembla.com/svn/v2engine/branches/TrunkFeatures@502 a1c503db-2e5a-4881-8658-c54c93b95141

---
+++ Server/Database/MySQL/SQL Files/Content/map_place_types.sql
@@ -0,0 +1,8 @@
+--
+-- Dumping data for table `map_place_types`
+--
+
+INSERT INTO `map_place_types` (`placeId`, `name`) VALUES
+('PLAC_00000000000000000000003', 'Bank'),
+('PLAC_00000000000000000000002', 'Shrine'),
+('PLAC_00000000000000000000001', 'Store');
---
+++ Server/Database/MySQL/SQL Files/Content/mastery_types.sql
@@ -0,0 +1,15 @@
+-- 01/15/2011
+-- Content for Mastery_types table
+
+INSERT INTO mastery_types VALUES(0,'Armor');
+INSERT INTO mastery_types VALUES(1,'Sword');
+INSERT INTO mastery_types VALUES(2,'Axe');
+INSERT INTO mastery_types VALUES(3,'Mace');
+INSERT INTO mastery_types VALUES(4,'Staff');
+INSERT INTO mastery_types VALUES(5,'Bow');
+INSERT INTO mastery_types VALUES(6,'Fire');
+INSERT INTO mastery_types VALUES(7,'Air');
+INSERT INTO mastery_types VALUES(8,'Cold');
+INSERT INTO mastery_types VALUES(9,'Earth');
+INSERT INTO mastery_types VALUES(10,'Shadow');
+INSERT INTO mastery_types VALUES(11,'Arcane');
