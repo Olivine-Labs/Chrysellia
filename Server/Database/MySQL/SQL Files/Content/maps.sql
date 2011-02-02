5b0e1ab74d7d48f6884fdc2a94a8c7e07f8d658b
BrokenWits <BrokenWits@a1c503db-2e5a-4881-8658-c54c93b95141>
2011-02-02 16:15:44 +0000

    Initial world map; cities and dungeons not added as of yet.
    git-svn-id: https://subversion.assembla.com/svn/v2engine/branches/TrunkFeatures@592 a1c503db-2e5a-4881-8658-c54c93b95141

--- Server/Database/MySQL/SQL Files/Content/maps.sql
+++ Server/Database/MySQL/SQL Files/Content/maps.sql
@@ -4,4 +4,4 @@
 
 INSERT INTO `maps` (`mapId`, `pvp`, `name`, `dimensionX`, `dimensionY`) VALUES
 ('MAP_00000000000000000000001', '\0', 'Test City', 5, 5),
-('MAP_00000000000000000000002', '\0', 'Wilderness', 20, 20);
+('MAP_00000000000000000000002', '\0', 'Wilderness', 180, 100);
