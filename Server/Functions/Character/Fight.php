2285d73439e00908a77cd4a9309d124407f13d6c
dditthardt <dditthardt@a1c503db-2e5a-4881-8658-c54c93b95141>
2011-02-18 03:33:10 +0000

    git-svn-id: https://subversion.assembla.com/svn/v2engine/trunk@666 a1c503db-2e5a-4881-8658-c54c93b95141

--- Server/Functions/Character/Fight.php
+++ Server/Functions/Character/Fight.php
@@ -20,84 +20,94 @@ if(
 ){
 	if(($Get->FightType == 0) || ($Get->FightType == 1))
 	{
-		$Character = new \Entities\Character();
-		$Character->CharacterId = $_SESSION['CharacterId'];
-		if($Database->Characters->LoadPosition($Character))
+		$Map = new \Entities\Map();
+		$Map->MapId = $Character->MapId;
+		$Cell = $Database->Maps->LoadCell($Map, $Get->X, $Get->Y);
+		if($Cell['Pvp'])
 		{
-			$EnemyCharacter = new \Entities\Character();
-			$EnemyCharacter ->CharacterId = $Get->CharacterId;
-			if($Database->Characters->LoadPosition($EnemyCharacter))
+			$Character = new \Entities\Character();
+			$Character->CharacterId = $_SESSION['CharacterId'];
+			if($Database->Characters->LoadPosition($Character))
 			{
-				if(
-					($Character->MapId == $EnemyCharacter->MapId) &&
-					($Character->PositionX == $EnemyCharacter->PositionX) &&
-					($Character->PositionY == $EnemyCharacter->PositionY)
-				){
+				$EnemyCharacter = new \Entities\Character();
+				$EnemyCharacter ->CharacterId = $Get->CharacterId;
+				if($Database->Characters->LoadPosition($EnemyCharacter))
+				{
 					if(
-						$Database->Characters->LoadTraits($Character) &&
-						$Database->Characters->LoadTraits($EnemyCharacter) &&
-						is_array($Character->Masteries = $Database->Characters->LoadMasteries($Character)) &&
-						is_array($EnemyCharacter->Masteries = $Database->Characters->LoadMasteries($EnemyCharacter))
+						($Character->MapId == $EnemyCharacter->MapId) &&
+						($Character->PositionX == $EnemyCharacter->PositionX) &&
+						($Character->PositionY == $EnemyCharacter->PositionY)
 					){
-						$Race = new \Entities\Race();
-						$Race->RaceId = $Character->RaceId;
-						if(is_array($Character->RacialMasteries = $Database->Races->LoadDefaultMasteries($Race)))
-						{
-							if(($Character->Health > 0) && ($EnemyCharacter->Health > 0) && ($EnemyCharacter->Level >= $Character->Level*0.75))
+						if(
+							$Database->Characters->LoadTraits($Character) &&
+							$Database->Characters->LoadTraits($EnemyCharacter) &&
+							is_array($Character->Masteries = $Database->Characters->LoadMasteries($Character)) &&
+							is_array($EnemyCharacter->Masteries = $Database->Characters->LoadMasteries($EnemyCharacter))
+						){
+							$Race = new \Entities\Race();
+							$Race->RaceId = $Character->RaceId;
+							if(is_array($Character->RacialMasteries = $Database->Races->LoadDefaultMasteries($Race)))
 							{
-								$SameMonster = false;
-								$Character->Equipment = $Database->Items->LoadEquippedItems($Character);
-								$EnemyCharacter->Equipment = $Database->Items->LoadEquippedItems($EnemyCharacter);
-
-								if($AttackResult = $Character->Attack($EnemyCharacter, $Get->FightType))
+								if(($Character->Health > 0) && ($EnemyCharacter->Health > 0) && ($EnemyCharacter->Level >= $Character->Level*0.75))
 								{
-									$Success = true;
-									$Database->startTransaction();
-									if($Database->Characters->UpdateTraits($Character) && $Database->Characters->UpdateTraits($EnemyCharacter))
-									{
-										foreach($AttackResult['Masteries'] AS $MasteryId)
-										{
-											if(!$Database->Characters->UpdateMastery($Character, $MasteryId, $Character->Masteries[$MasteryId]['Value'], $Character->Masteries[$MasteryId]['Bonus']))
-											{
-												$Success = false;
-												break;
-											}
-										}
+									$SameMonster = false;
+									$Character->Equipment = $Database->Items->LoadEquippedItems($Character);
+									$EnemyCharacter->Equipment = $Database->Items->LoadEquippedItems($EnemyCharacter);	
 
-										if($Success)
+									if($AttackResult = $Character->Attack($EnemyCharacter, $Get->FightType))
+									{
+										$Success = true;
+										$Database->startTransaction();
+										if($Database->Characters->UpdateTraits($Character) && $Database->Characters->UpdateTraits($EnemyCharacter))
 										{
-											$Database->Characters->LoadById($Character);
-											$Message['AttackedBy'] = $Character->Name;
-											$Message['BattleData'] = $AttackResult;
-											$Message['MessageType'] = 1;
-											if(!$Database->Chat->Insert($Character, 'CHAN_00000000000000000000001', $Message, 255, $EnemyCharacter))
+											foreach($AttackResult['Masteries'] AS $MasteryId)
 											{
-												$Success = false;
+												if(!$Database->Characters->UpdateMastery($Character, $MasteryId, $Character->Masteries[$MasteryId]['Value'], $Character->Masteries[$MasteryId]['Bonus']))
+												{
+													$Success = false;
+													break;
+												}
+											}
+
+											if($Success)
+											{
+												$Database->Characters->LoadById($Character);
+												$Message['AttackedBy'] = $Character->Name;
+												$Message['BattleData'] = $AttackResult;
+												$Message['MessageType'] = 1;
+												if(!$Database->Chat->Insert($Character, 'CHAN_00000000000000000000001', $Message, 255, $EnemyCharacter))
+												{
+													$Success = false;
+													$Response->Set('Result', \Protocol\Response::ER_DBERROR);
+												}
+											}
+											else	
+											{
 												$Response->Set('Result', \Protocol\Response::ER_DBERROR);
 											}
 										}
-										else	
+										else
 										{
 											$Response->Set('Result', \Protocol\Response::ER_DBERROR);
 										}
-									}
-									else
-									{
-										$Response->Set('Result', \Protocol\Response::ER_DBERROR);
-									}
-
-									if($Success)
-									{
-										$Response->Set('Result', \Protocol\Response::ER_SUCCESS);
-										$Response->Set('Data', $AttackResult);
-										$_SESSION['NextAction'] = microtime(true) + 1.50;
-										$Database->commitTransaction();
-									}
-									else
-									{
-										$Database->rollbackTransaction();
-									}
-								}	
+	
+										if($Success)
+										{
+											$Response->Set('Result', \Protocol\Response::ER_SUCCESS);
+											$Response->Set('Data', $AttackResult);
+											$_SESSION['NextAction'] = microtime(true) + 1.50;
+											$Database->commitTransaction();
+										}
+										else
+										{
+											$Database->rollbackTransaction();
+										}
+									}	
+								}
+							}
+							else
+							{
+								$Response->Set('Result', \Protocol\Response::ER_DBERROR);
 							}
 						}
 						else
@@ -107,7 +117,7 @@ if(
 					}
 					else
 					{
-						$Response->Set('Result', \Protocol\Response::ER_DBERROR);
+						$Response->Set('Result', \Protocol\Response::ER_BADDATA);
 					}
 				}
 				else
@@ -117,12 +127,12 @@ if(
 			}
 			else
 			{
-				$Response->Set('Result', \Protocol\Response::ER_BADDATA);
+				$Response->Set('Result', \Protocol\Response::ER_DBERROR);
 			}
 		}
 		else
 		{
-			$Response->Set('Result', \Protocol\Response::ER_DBERROR);
+			$Response->Set('Result', \Protocol\Response::ER_BADDATA);
 		}
 	}
 	else
