8927a3dea2c910ac5ae797f04da98cca722af582
ajacksified <ajacksified@a1c503db-2e5a-4881-8658-c54c93b95141>
2011-02-19 03:09:58 +0000

    moved the files into the branch, because I herpderped by checking into trunk.
    git-svn-id: https://subversion.assembla.com/svn/v2engine/branches/Drops@674 a1c503db-2e5a-4881-8658-c54c93b95141

--- WebClient/js/startup.js
+++ WebClient/js/startup.js
@@ -8,10 +8,10 @@ $(function(){
 		url: V2Core.SERVERCODE_DIRECTORY + "index.php",
 		dataType: "json",
 		global: false,
-		timeout: 1500
+		timeout: 3333
 	});
 
-	vc.CheckVersion(function(v){ if(v != vc.Version) { alert("Your game file cache is out of date.\nPlease clear your browser's cache."); }  });
+	vc.CheckVersion(function(v){ if(v != vc.Version) { $.jStorage.flush(); alert("Your game file cache is out of date.\nPlease clear your browser's cache."); }  });
 
 	$("a").focusin(function() {
 	  $(this).css("opacity",.75);
